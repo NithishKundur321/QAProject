255412e6a62a041887e67300e646e04aba65be59
Nithish Kundur <nithishreddy36@gmail.com>
2024-07-27 23:05:06 +0530

    Pushed the Updated code

--- src/main/java/com/tutorialsninja/automation/pages/RegisterPage.java
+++ src/main/java/com/tutorialsninja/automation/pages/RegisterPage.java
@@ -1,10 +1,22 @@
 package com.tutorialsninja.automation.pages;
 
+import java.util.Map;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.tutorialsninja.automation.base.Base;
+import com.tutorialsninja.automation.framework.Elements;
+
+import cucumber.api.DataTable;
 
 public class RegisterPage 
 {
+	public RegisterPage()
+	{
+		PageFactory.initElements(Base.driver, this);
+	}
 	@FindBy(id="input-firstname")
 	public static WebElement firstName;
 	
@@ -14,11 +26,31 @@ public class RegisterPage
 	@FindBy(id="input-email")
 	public static WebElement email;
 	
+	@FindBy(id="input-telephone")
+	public static WebElement telephone;
+	
 	@FindBy(id="input-password")
 	public static WebElement password;
 	
 	@FindBy(id="input-confirm")
 	public static WebElement confirmPassword;
+	
+	@FindBy(id="agree")
+	public static WebElement privacyPolicy;
+	
+	@FindBy(id="input[type='submit'][value='Continue']")
+	public static WebElement continueButton;
+	
+	public static void enterAllDetails(DataTable dataTable)
+	{
+		Map<String,String> map =dataTable.asMap(String.class, String.class);
+		Elements.TypeText(RegisterPage.firstName, map.get("FirstName"));
+		Elements.TypeText(RegisterPage.lastname, map.get("LastName"));
+		Elements.TypeText(RegisterPage.email, map.get("Email"));
+		Elements.TypeText(RegisterPage.telephone, map.get("Telephone"));
+		Elements.TypeText(RegisterPage.password, map.get("Password"));
+		Elements.TypeText(RegisterPage.confirmPassword, map.get("ConfirmPassword"));
+	}
 	
 
 }
--- src/test/java/com/tutorialsninja/automation/stepdef/Register.java
+++ src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -1,14 +1,26 @@
 package com.tutorialsninja.automation.stepdef;
 
+import java.util.Map;
+
+import org.junit.Assert;
+
 import com.tutorialsninja.automation.base.Base;
 import com.tutorialsninja.automation.framework.Elements;
+import com.tutorialsninja.automation.pages.AccountSuccessPage;
 import com.tutorialsninja.automation.pages.HeadersSection;
+import com.tutorialsninja.automation.pages.RegisterPage;
 
 import cucumber.api.DataTable;
 import cucumber.api.java.en.*;
 
+
+
+
 public class Register 
 {
+	HeadersSection headersSection = new HeadersSection();
+	RegisterPage registerPage = new RegisterPage();
+	AccountSuccessPage accountsuccess = new AccountSuccessPage();
 	@Given("^I launch the application$")
 	public void i_launch_the_application()  
 	{
@@ -25,22 +37,34 @@ public class Register
 	}
 
 	@When("^I provide all the below valid details$")
-	public void i_provide_all_the_below_valid_details(DataTable dataTable)  {
+	public void i_provide_all_the_below_valid_details(DataTable dataTable)
+	{
+		RegisterPage.enterAllDetails(dataTable);
+		
+		
+		
 	   
 	}
 
 	@And("^I select the Privacy Policy$")
-	public void i_select_the_Privacy_Policy()  {
-	   
+	public void i_select_the_Privacy_Policy()
+	{
+		Elements.click(RegisterPage.privacyPolicy);
 	}
 
 	@And("^I click on Continue button$")
-	public void i_click_on_Continue_button()  {
+	public void i_click_on_Continue_button()  
+	{
+		Elements.click(RegisterPage.continueButton);
 	   
 	}
 
 	@Then("^I should see that the User Account has successfully created$")
-	public void i_should_see_that_the_User_Account_has_successfully_created()  {
+	public void i_should_see_that_the_User_Account_has_successfully_created()
+	{
+		Assert.assertTrue(Elements.isDisplayed(AccountSuccessPage.successBreadcrumb));
+		
+		
 	   
 	}
 
